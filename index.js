174cb1c504b0f886d14ba2329718e1e4d291d301
wuriyanto <wuriyanto48@yahoo.co.id>
2019-02-28 23:04:28 +0700

    construct user module

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 'use strict';
 
+const Sequelize = require('sequelize');
+
 const Server = require('./lib/servers/server');
 const Database = require('./lib/database/database');
+const UserModel = require('./lib/modules/user/model/user');
+const UserHandler = require('./lib/modules/user/handler/handler');
 
 // load env
 require('dotenv').config();
@@ -26,7 +30,19 @@ db.connection.authenticate()
     console.error('Unable to connect to the database:', err);
 });
 
-const server = new Server();
+db.connection.sync({ force: true })
+  .then(() => {
+    console.log(`Database & tables created!`)
+  })
+
+const userModel = UserModel(db.connection, Sequelize);
+const userHandler = UserHandler(userModel);
+
+const handlers = {
+    userHandler: userHandler
+};
+
+const server = new Server(handlers);
 
 server.app.listen(PORT, err => {
     if (err) {
