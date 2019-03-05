aa8398481fc2345d41bd97131ae655a6c009a766
wuriyanto <wuriyanto48@yahoo.co.id>
2019-03-05 21:40:58 +0700

    add jwt gen test

---
+++ test/jwt/jwt.test.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const jwtGen = require('../../lib/shared/jwt_gen');
+const Key = require('../../lib/shared/key');
+
+test('should return valid jwt', ( )=> {
+
+    // set jwt options
+    const jwtOptions = {
+        issuer: "piyelek.github.io",
+        audience: "1111",
+        expired: "1m",
+        subject: "001"
+    };
+
+    // load private key
+    const privateKEY = Key.getKeySync('../../config/app.rsa');
+
+    const token = jwtGen(privateKEY, {"name": "piyelek"}, jwtOptions);
+
+    expect(token).not.toBe('');
+
+});
